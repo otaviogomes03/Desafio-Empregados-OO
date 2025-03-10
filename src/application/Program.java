6a4786dfdcfa8573b0c389045dc9c3bb536acfe9
Otávio Gomes <otaviogomes0303@gmail.com>
2025-03-10 13:29:21 -0300

    First commit

---
+++ src/application/Program.java
@@ -0,0 +1,65 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Address;
+import entities.Department;
+import entities.Employee;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Nome do departamento: ");
+		String departmentName = sc.nextLine();
+		System.out.print("Dia do pagamento: ");
+		int payDay =  sc.nextInt();
+		System.out.print("Email: ");
+		sc.nextLine();
+		String email = sc.nextLine();
+		System.out.print("Telefone: ");
+		String phone = sc.nextLine();
+		
+		Address address = new Address(email, phone);
+		
+		System.out.print("Quantos funcionários tem o departamento? ");
+		int N = sc.nextInt();
+		
+		Department department = new Department(departmentName, payDay, address);
+		
+		for (int i = 1; i <= N; i++) {
+			System.out.println("Dados do funcionário " + i + ":");
+			System.out.print("Nome: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salário: ");
+			double salary = sc.nextDouble();	
+			
+			Employee emp = new Employee(name, salary);
+			
+			department.addEmployee(emp);
+		}
+		
+		System.out.println();
+		
+		showReport(department);
+		
+		sc.close();
+	}
+	
+	public static void showReport(Department department) {
+		System.out.println("FOLHA DE PAGAMENTO:");
+		System.out.printf("Departamento %s = R$ %.2f\n", department.getName(), department.payrrol());
+		System.out.println("Pagamento realizado no dia " + department.getPayDay());
+		System.out.println("Funcionários: ");
+		for (Employee emp : department.getEmployees()) {
+			System.out.println(emp.getName());
+		}
+		System.out.println("Para dúvidas favor entrar em contato: " + department.getAdress().getEmail());
+	}
+
+}
---
+++ src/entities/Department.java
@@ -0,0 +1,66 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+	
+	private String name;
+	private Integer payDay;
+	private Address adress;
+    private List<Employee> employees = new ArrayList<>();
+    
+    public Department() {
+    }
+
+	public Department(String name, Integer payDay, Address adress) {
+		this.name = name;
+		this.payDay = payDay;
+		this.adress = adress;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPayDay() {
+		return payDay;
+	}
+
+	public void setPayDay(Integer payDay) {
+		this.payDay = payDay;
+	}
+
+	public Address getAdress() {
+		return adress;
+	}
+
+	public void setAdress(Address adress) {
+		this.adress = adress;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void addEmployee(Employee employee) {
+		employees.add(employee);
+	}
+	
+	public void removeEmployee(Employee employee) {
+		employees.remove(employee);
+	}
+	
+	public Double payrrol() {
+		double sum = 0;
+		for (Employee emp : employees) {
+			sum += emp.getSalary();
+		}
+		return sum;
+	}
+    
+}
